fb12a5b1e1a153cbcbe8053a1b00e6c3b0011343
Orlando <orlandotelles129@gmail.com>
2025-09-29 19:39:56 -0300

    Delete pdf_report_threads directory

--- pdf_report_threads/gerar_csv.py
+++
@@ -1,8 +0,0 @@
-# gera_csv.py
-import csv
-
-with open('data/dados.csv', 'w', newline='', encoding='utf-8') as f:
-    writer = csv.writer(f)
-    writer.writerow(['ID', 'Nome', 'Valor'])
-    for i in range(1, 101):
-        writer.writerow([i, f'Item {i}', f'R${i * 10:.2f}'])
